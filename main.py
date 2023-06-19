5efdc194ee40e6c753b6a08c375d6b09548a91fb
iooner <contact@iooner.be>
2023-06-19 10:08:26 +0200

    add esc key

--- main.py
+++ main.py
@@ -61,12 +61,21 @@ def slide_back(channel):
         image_actuelle = len(images) - 1
     afficher_image()
 
+def killbaq(event):
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+        print("Arrêt du diaporama")
+        GPIO.cleanup()
+        pygame.quit()
+        sys.exit()
+
 # Ajout des gestionnaires d'événements pour les broches GPIO
 GPIO.add_event_detect(pin_go, GPIO.FALLING, callback=slide_next, bouncetime=300)
 GPIO.add_event_detect(pin_stop, GPIO.FALLING, callback=slide_back, bouncetime=300)
 
 try:
     while True:
+        for event in pygame.event.get():
+            quitter_diaporama(event)
         sleep(0.1)
 
 except KeyboardInterrupt:
