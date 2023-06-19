f5cfc7e210708967106c5d4b585f0e523021383a
iooner <contact@iooner.be>
2023-06-19 11:57:15 +0200

    Doing stuff

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ chemin_images = "/home/fmj/BAQPython/BAQFMJPython/slides/"
 # Liste des fichiers d'images présents dans le répertoire
 images = [f for f in os.listdir(chemin_images) if f.endswith(('.jpg', '.png', '.jpeg','.JPG', '.PNG', '.JPEG'))]
 images = sorted(images, key=lambda x: int(x.split('.')[0]))
+
+
 print(images)
 
 # Variable pour stocker l'index de l'image actuelle
@@ -45,8 +47,6 @@ screen.blit(image, (0, 0))
 pygame.display.flip()
 
 
-
-# Fonction pour passer à l'image suivante
 def slide_next(channel):
     global actual_slide
     print(actual_slide)
@@ -55,8 +55,6 @@ def slide_next(channel):
     print("Affichage du slide:", chemin_image)
     if(data["sound"] == True):
         sound.play()
-    
-    # Chargement et affichage de l'image
     image = pygame.image.load(chemin_image)
     image = pygame.transform.scale(image, (pygame.display.Info().current_w, pygame.display.Info().current_h))
     screen = pygame.display.get_surface()
@@ -68,7 +66,6 @@ def slide_next(channel):
     else:
         actual_slide = 0
 
-# Fonction pour revenir à l'image précédente
 def slide_stop(channel):
     global actual_slide
     actual_slide = 0
@@ -80,12 +77,11 @@ def slide_stop(channel):
 
 def killbaq(event):
     if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-        print("Arrêt du diaporama")
+        print("Stopping the BAQ")
         GPIO.cleanup()
         pygame.quit()
         sys.exit()
 
-# Ajout des gestionnaires d'événements pour les broches GPIO
 GPIO.add_event_detect(pin_go, GPIO.FALLING, callback=slide_next, bouncetime=300)
 GPIO.add_event_detect(pin_stop, GPIO.FALLING, callback=slide_stop, bouncetime=300)
 
@@ -96,6 +92,6 @@ try:
         sleep(0.1)
 
 except KeyboardInterrupt:
-    print("Arrêt du diaporama")
+    print("Stopping the BAQ")
     GPIO.cleanup()
     pygame.quit()
