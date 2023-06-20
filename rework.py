63771e786130dc7577050434c10c26a843bbff80
iooner <contact@iooner.be>
2023-06-20 18:39:18 +0200

    test

--- rework.py
+++ rework.py
@@ -39,3 +39,10 @@ image = pygame.transform.scale(image, (pygame.display.Info().current_w, pygame.d
 screen = pygame.display.get_surface()
 screen.blit(image, (0, 0))
 pygame.display.flip()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
